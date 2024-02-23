f4fa5a189d20ee914ba07df175549dfbb5b7ef71
Caden McFate <cadenmcfate@gmail.com>
2024-02-23 16:41:56 -0700

    implimented conversion function

--- code.js
+++ code.js
@@ -1,3 +1,12 @@
 function convertToAdjList(adjMatrix) {
-    return adjMatrix;
+    let adjList = [];
+    for (let u = 0; u < adjMatrix.length; u++) {
+	adjList[u] = [];
+	for (let v = 0; v < adjMatrix.length; v++) {
+	    if (adjMatrix[u][v]) {
+		adjList[u].push(v);
+	    }
+	}
+    }
+    return adjList;
 }
