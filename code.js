2f9dfb73806e26eed3c3b71b8d3eec9ced53cc76
Caden McFate <cadenmcfate@gmail.com>
2024-02-23 17:04:15 -0700

    implimented bonus

--- code.js
+++ code.js
@@ -10,3 +10,17 @@ function convertToAdjList(adjMatrix) {
     }
     return adjList;
 }
+
+function convertToAdjMatrix(adjList) {
+    let adjMatrix = [];
+    for (let u = 0; u < adjList.length; u++) {
+	adjMatrix[u] = [];
+	for (let i = 0; i < adjList.length; i++) adjMatrix[u][i] = 0;
+	for (let v = 0; v < adjList[u].length; v++) adjMatrix[u][adjList[u][v]] = 1;
+    }
+    return adjMatrix;
+}
+
+
+//console.log(convertToAdjList(convertToAdjMatrix([[1],[0],[0,1]])));
+
